3468ee23ca302c17440576a9bccbb7b6055dbcad
aya-alsagher <ayaalsagher4@gmail.com>
2026-04-14 16:25:48 -0700

    Update CitizenController

---
+++ app/Http/Controllers/CitizenController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Coupon;
+use App\Models\Medicine;
+use App\Models\Order;
+use App\Traits\ShefaaTrait;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+
+class CitizenController extends Controller
+{
+    
+}
