ae821fdbd483729f11d280b58f930019bb0874ff
raghad-jomaa <ragad.jomaa2002@gmail.com>
2026-05-28 11:19:34 +0300

    update

--- app/Http/Controllers/ExchangedAdController.php
+++ app/Http/Controllers/ExchangedAdController.php
@@ -1,8 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\ExchangeAd;
+use App\Models\Specialist;
+use App\Models\User;
+use App\Traits\ShefaaTrait;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
+
 
 class ExchangedAdController extends Controller
 {
--- app/Http/Controllers/SpecialistController.php
+++ app/Http/Controllers/SpecialistController.php
@@ -1,8 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\ExchangeAd;
+use App\Models\Specialist;
+use App\Traits\ShefaaTrait;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+
+
 
 class SpecialistController extends Controller
 {
